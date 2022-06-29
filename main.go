279ce601e0e84dffd2fd23d32b39626fe48002a2
Brodie Peif <brodiep21@hotmail.com>
2022-06-28 21:02:15 -0400

    pairing in progress

--- main.go
+++ main.go
@@ -45,20 +45,18 @@ func sum(a []int) int {
 //solve this problem
 func pair(a []int) [][]int {
 	arr := [][]int{}
-	for i := 0; i < len(a)*len(a); i++ {
+	size := len(a) * len(a)
+	counter := 0
+	for i := 0; i < size; i++ {
 		arr = append(arr, []int{})
 	}
-	count := len(a) - 1
-	for i, v := range a {
-		arr[i] = append(arr[i], v)
-		arr[i] = append(arr[i], v)
-		count++
-		for _, v2 := range a {
-			arr[count] = append(arr[count], v2)
-			arr[count] = append(arr[count], v2)
-			if count >= len(a)*len(a) {
-				break
-			}
+	for i := 0; i < len(a); i++ {
+		for j := 0; j < len(a); j++ {
+			arr[j] = append(arr[j], a[i])
+		}
+		counter++
+		if counter < size {
+			arr[counter] = append(arr[counter], a[i])
 		}
 	}
 	return arr
